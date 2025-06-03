6e745b7530ec74af38db119cd390515fdf822650
NicolasFumo <141787060+StarLogicSoftware@users.noreply.github.com>
2025-06-03 20:57:49 -0300

    .

---
+++ Practicas en clase/clase0306.cs
@@ -0,0 +1,81 @@
+
+//// Declarar una variable
+//int miVariable;
+
+//// Guardar un dato FIJO en la variable
+//miVariable = 20;
+
+//miVariable = int.Parse(Console.ReadLine());
+
+//Console.Write("El valor que hay dentro de la variable es: ");
+//Console.WriteLine(miVariable);
+
+// Pedir dos numeros por consola y mostrar su promedio
+//float nota1,nota2,resultado;
+
+//Console.WriteLine("Ingrese la primer nota: ");
+//nota1 = float.Parse(Console.ReadLine());
+
+//Console.WriteLine("Ingrese la segunda nota");
+//nota2 = float.Parse(Console.ReadLine());
+
+//resultado = (nota1 + nota2) / 2;
+
+//Console.Write("El promedio es: ");
+//Console.WriteLine(resultado);
+
+//int miVariable;
+
+//miVariable = 20;
+
+//if (miVariable > 7) // si 9 es mayor que 7
+//{
+//    Console.Write("Es mayor que 7 ");
+//}
+//else // sinó
+//{ // las llaves son para delimitar un bloque de código
+//    Console.Write("Es menor que 7 ");
+//}
+
+
+
+// EJERCICIO DE CONDICIONALES
+// Pedir la edad de una persona y notificar si
+// se puede incribir al curso de programación
+
+int edad;
+
+Console.WriteLine("Ingrese la edad del postulante: ");
+edad = int.Parse(Console.ReadLine());
+
+if (edad >= 15)
+{
+    Console.WriteLine("Se puede inscribir al INCREIBLE curso de programación");
+}
+else
+{
+    Console.WriteLine("No tiene edad suficiente para inscribirse");
+}
+
+// Bucles
+// Definido
+
+for (int contador = 0; contador < 10; contador++)
+{
+    // lineas a repetir una X cantidad de veces
+}
+
+// indefinidos
+// while (pregunta y hace) / do-while (hace y pregunta)
+
+while (2 > 7)
+{
+    // lineas a repetir
+}
+
+do
+{
+    // lineas a repetir
+} while (2 > 7);
+
+Console.WriteLine("aca termina todo");
--- Teoria de codigo/Sintaxis de código.md
+++ Teoria de codigo/Sintaxis de código.md
@@ -16,7 +16,27 @@ Reglas de ortografia del lenguaje
 - Cualquier error hace que el codigo no funcione
 - Hay errores de compilación y ejecución
 - Las llaves y parentesis se abren y se cierran en pares
+- Si es una llave no lleva ;
+- Si hay llaves, la linea **anterior** a la **apertura** de la llave no lleva ;
 
 ## Solucion vs programa (proyecto)
 
 Una solucion puede tener varios programas (como una caja con varios proyectos dentro)
+
+## Tipos de datos
+
+Enteros -> int
+Reales -> float
+Cadenas -> string
+Booleanos -> bool
+
+## Expresiones
+Es codigo que se tiene que procesar en el procesador y que puede producir un resultado.
+
+## Expresiones de condicionales
+**>** mayor que
+**>=** mayor o igual
+**<** menor que
+**<=** menor o igual
+**==** igual que
+**!=** diferente que
