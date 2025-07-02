bb77c994b79f79ced990bc49cb8a019a8d96723c
NicolasFumo <nicolasfumo@gmail.com>
2025-07-01 21:20:21 -0300

    Clase 0107

--- Practicas en clase/Clase 0107.cs
+++ Practicas en clase/Clase 0107.cs
@@ -1,9 +1,55 @@
-// Ejercicio 2
-
+// Ejercicio 2 - Practica de funciones
+// Link al documento > https://github.com/StarLogicSoftware/programacion/blob/main/Ejercicios/Practica%20de%20funciones.md
 
 float lado1, lado2;
 
 float resultado = CalcularPerimetro(5, 10);
+
+lado1 = SolicitarLado("A");
+lado2 = SolicitarLado("B");
+
+
+// Falta calcular el area y el perimetro y mostrar los resultados
+
+float SolicitarLado(string lado)
+{
+    float numero;
+
+    numero = solicitarFloat($"Ingrese el lado {lado}");
+
+    while (numero <= 0)
+    {
+        Console.WriteLine("El numero es incorrecto");
+
+        numero = solicitarFloat($"Ingrese el lado {lado}");
+    }
+
+    return numero;
+}
+
+// funcion dedicada exclusivamente a obtener un float
+float solicitarFloat(string mensaje)
+{
+    bool falla = false;
+    float numero = 0;
+
+    do
+    {
+        try
+        {
+            Console.WriteLine(mensaje);
+            numero = float.Parse(Console.ReadLine());
+            falla = false;
+        }
+        catch (Exception)
+        {
+            falla = true;
+        }
+
+    } while (falla == true);
+
+    return numero;
+}
 
 
 float CalcularArea(float ladoA, float ladoB)
@@ -21,23 +67,6 @@ void MostrarResultados(float area, float perimetro, float ladoA, float ladoB)
     Console.WriteLine($"El rectangulo de lado a={ladoA} y lado b={ladoB} tiene un área de {area}cm2 y " +
         $"un perímetro de {perimetro}cm");
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 /* EJERCICIO 1
 float cotizacion = 1240; // Cotización del dólar
