7a63ab5cd48aa2fd9ee3fd24393b2bf6c54577b8
NicolasFumo <141787060+StarLogicSoftware@users.noreply.github.com>
2025-09-16 21:03:34 -0300

    Clase 16/09

---
+++ Practicas en clase/Clase1609/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using Clase1609.Clases;
+using System.Text;
+using System.Windows;
+
+namespace Clase1609
+{
+    public partial class MainWindow : Window
+    {
+        // Aca se guardan los clientes en el sistema (como una mini base de datos)
+        List<Cliente> clientes = new List<Cliente>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegistrar_Click(object sender, RoutedEventArgs e)
+        {
+            /*
+            Crear un cliente (un objeto cliente)
+            Cargar los datos de la ventana en el objeto
+            Agregar ese objeto a la lista de clientes
+            Actualizar el datagrid con los clientes de la lista
+            */
+            Cliente c = new Cliente();
+
+            c.Nombre = txtNombre.Text;
+            c.Direccion = txtDireccion.Text;
+            c.Telefono = txtTelefono.Text;
+
+            clientes.Add(c);
+
+            dtgClientes.ItemsSource = null; // forzamos al datagrid a volver a leer la lista
+            dtgClientes.ItemsSource = clientes;
+        }
+
+        private void btnInformacion_Click(object sender, RoutedEventArgs e)
+        {
+            //Cliente clienteSeleccionado = (Cliente)dtgClientes.SelectedItem;
+            Cliente clienteSeleccionado = dtgClientes.SelectedItem as Cliente;
+
+            if (clienteSeleccionado == null)
+            {
+                MessageBox.Show("Debe seleccionar primero un cliente");
+            }
+            else
+            {
+                MessageBox.Show($"Cliente seleccionado: {clienteSeleccionado.Nombre}", "cliente");
+            }
+        }
+    }
+}
